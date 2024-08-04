8394c449b24f07906137a6ecd452c856c56a41c9
ChenQY <ChineseUserChen@163.com>
2024-08-04 14:53:23 +0800

    project codes-main.cpp

--- project_codes/main.cpp
+++ project_codes/main.cpp
@@ -1,3 +1,16 @@
+/**
+ * This C++ program prints "Hello SLAM" to the console.
+ * 
+ * @param argc The `argc` parameter in the `main` function stands for "argument count" and represents
+ * the number of arguments passed to the program when it is executed from the command line. It includes
+ * the name of the program itself as the first argument.
+ * @param argv argv is a pointer to an array of C-strings (char*) representing the command-line
+ * arguments passed to the program when it is executed. The first element (argv[0]) usually contains
+ * the name of the program itself. The argc parameter represents the number of arguments passed to the
+ * program.
+ * 
+ * @return The `main` function is returning an integer value of 0.
+ */
 #include <iostream>
 
 using namespace std;
